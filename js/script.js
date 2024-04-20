b00725512d282db301b516020dfadc9e38a96869
Phillip Logan <philo3194@gmail.com>
2024-04-19 22:25:53 -0500

    made changes to complete the functionality

--- js/script.js
+++ js/script.js
@@ -3,43 +3,57 @@ var products = [
     productId: 1,
     name: "unlimited social",
     allotment: "24GB",
-    price: "$2400.00",
+    price: "$2100.00",
   },
   {
     productId: 2,
     name: "unlimited youtube",
     allotment: "24GB",
-    price: "$2400.00",
+    price: "$2200.00",
   },
   {
     productId: 3,
     name: "unlimited instagram",
     allotment: "24GB",
-    price: "$2400.00",
+    price: "$2300.00",
   },
 ];
 
 window.onload = showPlans;
 
-function showPlan(product) {
+function addToCart(productID) {
+  var currentCart = localStorage.getItem("products");
+
+  if (currentCart == null) {
+    currentCart = "";
+  }
+
+  localStorage.setItem("products", currentCart + "," + productID);
+}
+
+function showPlan(productId) {
   var productContainer = document.getElementById("product-container");
 
-  var name = document.createElement("div");
-  name.innerHTML = product.name;
+  for (var product of products) {
+    if (product.productId == productId) {
+      var name = document.createElement("div");
+      name.innerHTML = product.name;
 
-  var price = document.createElement("span");
-  price.innerHTML = product.price;
+      var price = document.createElement("span");
+      price.innerHTML = product.price;
 
-  productContainer.appendChild(name);
-  productContainer.appendChild(price);
+      productContainer.appendChild(name);
+      productContainer.appendChild(price);
+    }
+  }
 }
 
 function showPlans() {
-  var shoppingCart = localStorage.getItem("products");
+  var shoppingCart = localStorage.getItem("products"); //,1,2,3
   console.log(shoppingCart);
 
   var productIdList = shoppingCart.split(",");
   console.log(productIdList);
 
-  products.forEach(showPlan);
+  productIdList.forEach(showPlan);
 }
